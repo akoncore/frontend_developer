3ac3a18ff4fb070a06671aeae9c9ec0859d9503a
aconcore <akniet.talgat.05@mail.ru>
2026-09-24 15:09:08 +0500

    practice_2_change

--- react_practice_2/about_me/src/data.ts
+++ react_practice_2/about_me/src/data.ts
@@ -3,8 +3,8 @@ import photo from "./assets/avatar.svg";
 import type { Profile, AboutData, ContactItem } from './types';
 
 export const profile: Profile = {
-  name: "Your Name",
-  role: "Student & future developer",
+  name: "Abdresh Akniet",
+  role: "Student of KBTU",
   tagline: "I'm learning React and building things that make me smile.",
   photo,
   photoAlt: "Portrait of Your Name",
@@ -12,15 +12,15 @@ export const profile: Profile = {
 
 export const about: AboutData = {
   paragraphs: [
-    "Hi! I'm a student from Kazakhstan. Write two or three sentences here: who you are, what you study, what you enjoy.",
-    "Add something fun: a hobby, a favourite game, a goal for this year.",
+    "Hi! My name is Akon. I am a student and a beginner software developer. I am interested in web development and programming.",
+    "Currently, I am learning React, TypeScript, Flutter, and Django. I enjoy learning new technologies and improving my skills.",
   ],
   skills: ["HTML", "CSS", "JavaScript", "TypeScript", "React", "Git"],
 };
 
 
 export const contacts: ContactItem[] = [
-  { label: "GitHub", value: "github.com/your-username", href: "https://github.com/your-username" },
-  { label: "Instagram", value: "@your_handle", href: "https://instagram.com/your_handle" },
-  { label: "Address", value: "Planet Earth" },
+  { label: "GitHub", value: "github.com/akoncore", href: "https://github.com/akoncore" },
+  { label: "Instagram", value: "@akon_abdresh", href: "https://www.instagram.com/akon_abdresh" },
+  { label: "Address", value: "NARYNQOL" },
 ];
